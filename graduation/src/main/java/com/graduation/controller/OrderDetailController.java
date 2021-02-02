c19e0478fa2607b272f43aa33e7f0b54d5b587f6
1291334282@qq.com <email>
2021-02-02 13:37:14 +0800

    后端

--- graduation/src/main/java/com/graduation/controller/OrderDetailController.java
+++ graduation/src/main/java/com/graduation/controller/OrderDetailController.java
@@ -1,8 +1,12 @@
 package com.graduation.controller;
 
 
+import com.graduation.entity.Orders;
 import com.graduation.entity.ResultUtil;
 import com.graduation.service.OrderDetailService;
+import com.graduation.service.OrderService;
+import com.graduation.service.UserService;
+import com.graduation.vo.OrderDetailVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -10,9 +14,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 叼大
@@ -26,13 +33,31 @@ public class OrderDetailController {
     @Autowired
     private OrderDetailService orderDetailService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private OrderService orderService;
+
     Logger log = LoggerFactory.getLogger(OrderDetailController.class);
 
     @ApiOperation("功能：查找对应订单的全部商品信息,备注（需要传入token,订单id）")
     @GetMapping("/findOrdersDetail")
-    public ResultUtil selectorderDetail(@RequestHeader("token") String token,@RequestParam(value = "id", required = true) Integer id) {
-        log.info("进入查找全部订单接口");
+    public ResultUtil selectorderDetail(@RequestHeader("token") String token, @RequestParam(value = "id", required = true) Integer id) {
+        log.info("进入查找对应订单接口");
         return ResultUtil.success(orderDetailService.selestOrderDetail(id));
+    }
+
+    @ApiOperation("功能：查找对应登录人的全部订单详情,备注（需要传入token）")
+    @GetMapping("/findOrdersAll")
+    public ResultUtil selectorderAll(@RequestHeader("token") String token) {
+        log.info("进入查找全部订单详情接口");
+        List<Orders> orders = orderService.selectorderbyuserid(userService.findByToken(token).getUserId());
+        List<List<OrderDetailVO>> list = new ArrayList<>();
+        for (Orders order : orders) {
+            list.add(orderDetailService.selestOrderDetail(order.getId()));
+        }
+        return ResultUtil.success(list);
     }
 }
 
