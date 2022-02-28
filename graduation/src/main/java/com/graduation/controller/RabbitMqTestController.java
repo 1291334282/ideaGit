abb261a5ae16091091de0864d86d9c524c08d923
hrz <65646439+1291334282@users.noreply.github.com>
2022-02-28 14:52:47 +0800

    rabbitmq

--- graduation/src/main/java/com/graduation/controller/RabbitMqTestController.java
+++ graduation/src/main/java/com/graduation/controller/RabbitMqTestController.java
@@ -12,7 +12,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/rabbit")
@@ -76,7 +77,47 @@ public class RabbitMqTestController {
         }
         List<Area> list = areaMapper.selectList(queryWrapper);
         long end = System.currentTimeMillis();
-        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+//        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+        areaMapper.createDataBase();
     }
 
+    private static int taskCount = 50;//任务数
+    private static AtomicInteger taskCountExecuted;//实际完成任务数
+
+    @PostMapping("/threed")
+    public void threed() {
+        taskCountExecuted = new AtomicInteger();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10,//核心线程数
+                20,//最大线程数
+                5,//非核心回收超时时间
+                TimeUnit.SECONDS,//超时时间单位
+                new ArrayBlockingQueue<>(30)//任务队列
+        );
+        System.out.println("总任务数：" + taskCount);
+        long start = System.currentTimeMillis();
+        //模拟任务提交
+        for (int i = 0; i < taskCount; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(500);//模拟执行耗时
+                    System.out.println("已执行" + taskCountExecuted.addAndGet(1) + "个任务");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            try {
+                //注意这里我try起来了，默认拒绝策略会报错
+                executor.execute(thread);
+            } catch (RejectedExecutionException e) {
+                taskCount = executor.getActiveCount() + executor.getQueue().size();
+            }
+        }
+        long end = 0;
+        while (executor.getCompletedTaskCount() < taskCount) {
+            end = System.currentTimeMillis();
+        }
+        System.out.println(taskCountExecuted + "个任务已执行,总耗时：" + (end - start));
+        executor.shutdown();
+    }
 }
