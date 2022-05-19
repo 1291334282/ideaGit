2d28fb54f72eb42bcc9aaa9bea59df54766d16ac
hrz <65646439+1291334282@users.noreply.github.com>
2022-05-19 09:11:45 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/SemaphoreDemo.java
+++ locks/src/main/java/com/lock/locks/demo/SemaphoreDemo.java
@@ -1,2 +1,27 @@
-package com.lock.locks.demo;public class SemaphoreDemo {
+package com.lock.locks.demo;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        //停车位为3个
+        Semaphore semaphore = new Semaphore(3);
+        for (int i = 1; i <= 6; i++) {
+            int finalI = i;
+            new Thread(()->{
+                try {
+                    semaphore.acquire(); //得到
+                    //抢到车位
+                    System.out.println(Thread.currentThread().getName()+" 抢到了车位{"+ finalI +"}");
+                    TimeUnit.SECONDS.sleep(2); //停车2s
+                    System.out.println(Thread.currentThread().getName()+" 离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();//释放
+                }
+            },String.valueOf(i)).start();
+        }
+    }
 }
