857c97b556f2961a8e736059c4539afe15668cfc
hrz <65646439+1291334282@users.noreply.github.com>
2022-07-25 09:55:14 +0800

    no message

--- graduation/src/main/java/com/graduation/controller/RabbitMqTestController.java
+++ graduation/src/main/java/com/graduation/controller/RabbitMqTestController.java
@@ -5,14 +5,25 @@ import com.graduation.config.Sender;
 import com.graduation.config.Task;
 import com.graduation.entity.Area;
 import com.graduation.entity.Test;
+import com.graduation.entity.User;
 import com.graduation.handler.FuzzyQuery;
 import com.graduation.mapper.AreaMapper;
+import com.graduation.mapper.UserMapper;
 import io.swagger.annotations.Api;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
@@ -68,7 +79,7 @@ public class RabbitMqTestController {
         Calendar cal = Calendar.getInstance();
         //获取当前年份
         int year = cal.get(Calendar.YEAR);
-        String firstDay=year+"-1-01";
+        String firstDay = year + "-1-01";
         System.out.println(firstDay);
     }
 
@@ -130,7 +141,9 @@ public class RabbitMqTestController {
         System.out.println(taskCountExecuted + "个任务已执行,总耗时：" + (end - start));
         executor.shutdown();
     }
+
     private static int num = 20;
+
     @GetMapping("/test1")
 //    @Lock(key = "test1")
     public void test1() {
@@ -142,9 +155,43 @@ public class RabbitMqTestController {
         num--;
         System.out.println("还剩余：" + num);
     }
+
+    @Autowired
+    UserMapper userMapper;
+
     @PostMapping("/ttt")
-    @FuzzyQuery
-    public String ttt(@RequestBody Test test){
-        return test.getName();
+//    @FuzzyQuery
+    public User ttt(@RequestBody Test test) {
+
+        User user = userMapper.getUser();
+        return user;
+    }
+
+    @GetMapping("/netDownLoadNet")
+    public void netDownLoadNet(HttpServletResponse response) throws Exception {
+        String netAddress = "http://192.168.2.40:9680/api/datasvr/smapp/%E6%A1%82%E5%9F%8E%E8%A1%97%E9%81%93%E5%9F%8E%E5%B8%82%E5%A4%A7%E8%84%91%E4%B8%80%E6%9C%9F%E5%BB%BA%E8%AE%BE%E9%A1%B9%E7%9B%AE%E7%94%A8%E6%88%B7%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C.pdf";
+        String filename = "12.pdf";
+        boolean isOnLine = true;
+        URL url = new URL(netAddress);
+        URLConnection conn = url.openConnection();
+        InputStream inputStream = conn.getInputStream();
+
+        response.reset();
+        response.setContentType(conn.getContentType());
+        if (isOnLine) {
+// 在线打开方式 文件名应该编码成UTF-8
+            response.setHeader("Content-Disposition", "inline; filename=" + URLEncoder.encode(filename, "UTF-8"));
+        } else {
+//纯下载方式 文件名应该编码成UTF-8
+            response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(filename, "UTF-8"));
+        }
+
+        byte[] buffer = new byte[1024];
+        int len;
+        OutputStream outputStream = response.getOutputStream();
+        while ((len = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, len);
+        }
+        inputStream.close();
     }
 }
