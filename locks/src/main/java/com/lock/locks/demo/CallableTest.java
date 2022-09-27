b170453b7fc2a18cd474bd5422b91f3acd98b89f
hrz <65646439+1291334282@users.noreply.github.com>
2022-09-27 15:39:13 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/CallableTest.java
+++ locks/src/main/java/com/lock/locks/demo/CallableTest.java
@@ -1,2 +1,31 @@
-package com.lock.locks.demo;public class CallableTest {
+package com.lock.locks.demo;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        for (int i = 1; i < 10; i++) {
+//            new Thread(new Runnable()).start();
+//            new Thread(new FutureTask<>( Callable)).start();
+            MyThread thread= new MyThread();
+            //适配类：FutureTask
+            FutureTask<String> futureTask = new FutureTask<>(thread);
+            //放入Thread使用
+            new Thread(futureTask,String.valueOf(i)).start();
+            //获取返回值
+            String s = futureTask.get();
+            System.out.println("返回值："+ s);
+        }
+    }
+}
+
+class MyThread implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("Call:"+Thread.currentThread().getName());
+        return "String"+Thread.currentThread().getName();
+    }
 }
