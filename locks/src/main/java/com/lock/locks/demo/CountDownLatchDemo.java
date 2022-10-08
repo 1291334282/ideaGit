81e67ab84f7e00c3bd850410ab90619a77d16495
hrz <65646439+1291334282@users.noreply.github.com>
2022-10-08 15:33:46 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/CountDownLatchDemo.java
+++ locks/src/main/java/com/lock/locks/demo/CountDownLatchDemo.java
@@ -1,2 +1,24 @@
-package com.lock.locks.demo;public class CountDownLatchDemo {
+package com.lock.locks.demo;
+
+import java.util.concurrent.CountDownLatch;
+
+//这是一个计数器  减法
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        //总数是6
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        for (int i = 1; i <= 6 ; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+" Go out");
+                countDownLatch.countDown(); //每个线程都数量-1
+            },String.valueOf(i)).start();
+        }
+        countDownLatch.await();  //等待计数器归零  然后向下执行
+
+        System.out.println("close door");
+
+    }
+
 }
