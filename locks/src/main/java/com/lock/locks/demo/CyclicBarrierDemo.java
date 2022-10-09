959826cdb017f29571bb4747937afbdc4e5c7770
hrz <65646439+1291334282@users.noreply.github.com>
2022-10-09 14:33:40 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/CyclicBarrierDemo.java
+++ locks/src/main/java/com/lock/locks/demo/CyclicBarrierDemo.java
@@ -1,2 +1,30 @@
-package com.lock.locks.demo;public class CyclicBarrierDemo {
+package com.lock.locks.demo;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+
+        //主线程
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7,()->{
+            System.out.println("召唤神龙~");
+        });
+
+        for (int i = 1; i <= 7; i++) {
+            //子线程
+            int finalI = i;
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+" 收集了第 {"+ finalI+"} 颗龙珠");
+                try {
+                    cyclicBarrier.await(); //加法计数 等待
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+    }
 }
