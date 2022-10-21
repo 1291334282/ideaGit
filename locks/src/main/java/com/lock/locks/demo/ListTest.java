63be0101a5c69b7cfdef9f228359d1dbb727a948
hrz <65646439+1291334282@users.noreply.github.com>
2022-10-21 17:16:57 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/ListTest.java
+++ locks/src/main/java/com/lock/locks/demo/ListTest.java
@@ -1,2 +1,21 @@
-package com.lock.locks.demo;public class ListTest {
+package com.lock.locks.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+//java.util.ConcurrentModificationException 并发修改异常！
+public class ListTest {
+    public static void main(String[] args) {
+
+        List<Object> arrayList = new ArrayList<>();
+
+        for(int i=1;i<=10;i++){
+            new Thread(()->{
+                arrayList.add(UUID.randomUUID().toString().substring(0,5));
+                System.out.println(arrayList);
+            },String.valueOf(i)).start();
+        }
+
+    }
 }
