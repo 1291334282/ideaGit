feec5ed0a48f079587cdeff933b834694c061509
hrz <65646439+1291334282@users.noreply.github.com>
2022-10-25 10:55:53 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/casDemo.java
+++ locks/src/main/java/com/lock/locks/demo/casDemo.java
@@ -1,2 +1,26 @@
-package com.lock.locks.demo;public class casDemo {
+package com.lock.locks.demo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class casDemo {
+    //CAS : compareAndSet 比较并交换
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(2020);
+
+        System.out.println(atomicInteger.compareAndSet(2020, 2021));
+        System.out.println(atomicInteger.get());
+
+        //boolean compareAndSet(int expect, int update)
+        //期望值、更新值
+        //如果实际值 和 我的期望值相同，那么就更新
+        //如果实际值 和 我的期望值不同，那么就不更新
+        System.out.println(atomicInteger.compareAndSet(2021, 2020));
+        System.out.println(atomicInteger.get());
+
+        //因为期望值是2020  实际值却变成了2021  所以会修改失败
+        //CAS 是CPU的并发原语
+//        atomicInteger.getAndIncrement(); //++操作
+        System.out.println(atomicInteger.compareAndSet(2020, 2021));
+        System.out.println(atomicInteger.get());
+    }
 }
