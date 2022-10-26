4c83d2cc009c46ad3197558057bf2152b0043758
hrz <65646439+1291334282@users.noreply.github.com>
2022-10-26 10:13:33 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/ListTest2.java
+++ locks/src/main/java/com/lock/locks/demo/ListTest2.java
@@ -1,2 +1,21 @@
-package com.lock.locks.demo;public class ListTest2 {
+package com.lock.locks.demo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class ListTest2 {
+    public static void main(String[] args) {
+
+        List<Object> arrayList = Collections.synchronizedList(new ArrayList<>());
+
+        for(int i=1;i<=10;i++){
+            new Thread(()->{
+                arrayList.add(UUID.randomUUID().toString().substring(0,5));
+                System.out.println(arrayList);
+            },String.valueOf(i)).start();
+        }
+
+    }
 }
