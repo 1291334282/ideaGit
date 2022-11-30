5d4e3e3fbc1f2f149dacf034d40f8f2247b58263
hrz <65646439+1291334282@users.noreply.github.com>
2022-11-30 09:05:55 +0800

    no message

--- locks/src/main/java/com/lock/locks/demo/ListTest3.java
+++ locks/src/main/java/com/lock/locks/demo/ListTest3.java
@@ -1,2 +1,20 @@
-package com.lock.locks.demo;public class ListTest3 {
+package com.lock.locks.demo;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ListTest3 {
+    public static void main(String[] args) {
+
+        List<Object> arrayList = new CopyOnWriteArrayList<>();
+
+        for(int i=1;i<=10;i++){
+            new Thread(()->{
+                arrayList.add(UUID.randomUUID().toString().substring(0,5));
+                System.out.println(arrayList);
+            },String.valueOf(i)).start();
+        }
+
+    }
 }
